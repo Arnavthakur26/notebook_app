39917c5d195d4dbd32521069d3c3645473860f57
Arnav <arnav206@gmail.com>
2023-03-19 22:30:34 +0530

    added vercel.js

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,15 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
+
+if (process.env.NODE_ENV == "production") {
+  const path = require("path");
+
+  app.get("/", (req, res) => {
+    app.use(express.static(path.resolve(__dirname, "frontend", "build")));
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
