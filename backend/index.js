9b634d88bae892e438ab1b30abbe0897c2795ad9
Arnav <arnav206@gmail.com>
2023-03-19 22:54:45 +0530

    updated static files path

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV == "production") {
   const path = require("path");
 
   app.get("/", (req, res) => {
-    app.use(express.static(path.resolve(__dirname, "frontend", "build")));
+    app.use(express.static(path.resolve(__dirname, "..", "frontend", "build")));
     res.sendFile(
       path.resolve(__dirname, "..", "frontend", "build", "index.html")
     );
