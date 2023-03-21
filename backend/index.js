f79e4b43f7823e0c4613893bd87a19b5d60e1af9
Arnav <arnav206@gmail.com>
2023-03-21 11:19:48 +0530

    changed index.js

--- backend/index.js
+++ backend/index.js
@@ -15,16 +15,6 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
-if (process.env.NODE_ENV == "production") {
-  const path = require("path");
-
-  app.get("/", (req, res) => {
-    app.use(express.static(path.resolve(__dirname, "..", "frontend", "build")));
-    res.sendFile(
-      path.resolve(__dirname, "..", "frontend", "build", "index.html")
-    );
-  });
-}
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
